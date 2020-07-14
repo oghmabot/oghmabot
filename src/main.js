d03aa990e68957673190a96c21be7fa063f4ded3
aergnist <46282792+aergnist@users.noreply.github.com>
2020-07-14 16:10:45 +0200

    Various cleanup and removal of old db connections

--- src/main.js
+++ src/main.js
@@ -3,10 +3,8 @@
 const scripts = require('./scripts.js');
 
 // Importing installed and default packages provided by npm
-const {CommandoClient} = require('discord.js-commando');
+const { CommandoClient } = require('discord.js-commando');
 const path = require('path');
-const Enmap = require('enmap');
-const EnmapMongo = require('enmap-mongo');
 const assets = require('./assets/index')
 const update = assets.Arelith.Update;
 const serverStatus = assets.Arelith.Status;
@@ -15,26 +13,17 @@ const serverStatus = assets.Arelith.Status;
 module.exports = (config) => {
   // Instantiating the bot/client, some settings in curly brackets
   const bot = new CommandoClient({
-    owners: config._BOT_OWNERS,
-    commandPrefix: '-',
+    owners: process.env.BOT_OWNERS,
+    commandPrefix: config.prefix,
     disableEveryone: true,
     unknownCommandResponse: false
   });
 
   bot.assets = assets;
-
-
-  //creating the guild/server based map that will be used to configure settings
-  bot.settings = new Enmap({ provider: new EnmapMongo ({ name: 'settings', dbName: config._MONGODB_ENMAPDB, url: `mongodb://${config._MONGODB_USER}:${config._MONGODB_PASSWORD}@oghmacluster-shard-00-00-vqfed.mongodb.net:27017,oghmacluster-shard-00-01-vqfed.mongodb.net:27017,oghmacluster-shard-00-02-vqfed.mongodb.net:27017/${config._MONGODB_ENMAPDB}?ssl=true&replicaSet=OghmaCluster-shard-0&authSource=admin&retryWrites=true`})});
-  bot.updates = new Enmap({ provider: new EnmapMongo ({ name: 'updates', dbName: config._MONGODB_ENMAPDB, url: `mongodb://${config._MONGODB_USER}:${config._MONGODB_PASSWORD}@oghmacluster-shard-00-00-vqfed.mongodb.net:27017,oghmacluster-shard-00-01-vqfed.mongodb.net:27017,oghmacluster-shard-00-02-vqfed.mongodb.net:27017/${config._MONGODB_ENMAPDB}?ssl=true&replicaSet=OghmaCluster-shard-0&authSource=admin&retryWrites=true`})});
-  bot.servers = new Enmap({ provider: new EnmapMongo ({ name: 'servers', dbName: config._MONGODB_ENMAPDB, url: `mongodb://${config._MONGODB_USER}:${config._MONGODB_PASSWORD}@oghmacluster-shard-00-00-vqfed.mongodb.net:27017,oghmacluster-shard-00-01-vqfed.mongodb.net:27017,oghmacluster-shard-00-02-vqfed.mongodb.net:27017/${config._MONGODB_ENMAPDB}?ssl=true&replicaSet=OghmaCluster-shard-0&authSource=admin&retryWrites=true`})});
-
   bot.defaultSettings = {
-      announcementsState: false,
-      statusState: false
+    announcementsState: false,
+    statusState: false
   }
-
-  bot.mode = config._MONGODB_ENMAPDB.replace(/.*-/,'');
 
   // Registering various default settings as well as custom command groups
   bot.registry
@@ -46,16 +35,13 @@ module.exports = (config) => {
       ['arelith', 'Commands to check information on Arelith'],
     ])
     .registerDefaultGroups()
-    .registerDefaultCommands({ping: false, prefix: false, commandState: false})
+    .registerDefaultCommands({ ping: false, prefix: false, commandState: false })
     .registerCommandsIn(path.join(__dirname, 'commands'));
 
   (async function () {
-      await bot.settings.defer;
-      await bot.updates.defer;
-      await bot.servers.defer;
-      scripts.run(bot);
-      bot.login(config._BOT_TOKEN);
-      setInterval(update.checkForUpdate,30*1000,bot);
-      setInterval(serverStatus.updateServerStatus, 15*1000, bot);
+    scripts.run(bot);
+    bot.login(process.env.BOT_TOKEN);
+    setInterval(update.checkForUpdate, 30 * 1000, bot);
+    setInterval(serverStatus.updateServerStatus, 15 * 1000, bot);
   }());
 }
--- src/scripts.js
+++ src/scripts.js
@@ -3,39 +3,62 @@
 const Discord = require('discord.js');
 
 module.exports = {
-  run: function(bot) {
+  run: function (bot) {
+    /**
+     * When bot is ready, output logged in servers
+     * @ignore
+     */
     bot.on('ready', () => {
-      // Once the bot is ready, it outputs a list of active servers to console
-      console.log(`Logged in to servers: ${Array.from(bot.guilds.values())}`);
-      //bot.settings.set('528197859854647299', bot.defaultSettings);
-      const wb = bot.settings.get('528197859854647299', "statusWebhook");
+      console.log(loggedInServersToString(bot));
+
+      const wb = bot.settings.get('528197859854647299', "statusWebhook");      
       if (wb && bot.mode == "live") {
-          const embedOnline = new Discord.RichEmbed();
-          embedOnline.setColor(0x00ff00);
-          //embedOnline.setTitle("Oghmabot Online");
-          embedOnline.setDescription(`Logged in to servers: ${Array.from(bot.guilds.values())}`);
-          const wbClient = new Discord.WebhookClient(wb.id, wb.token);
-          wbClient.name = "Oghmabot Online";
-          wbClient.send("",embedOnline);
+        const embedOnline = loggedInServersToEmbed(bot);
+        const wbClient = new Discord.WebhookClient(wb.id, wb.token);
+        wbClient.name = "Oghmabot Online";
+        wbClient.send("", embedOnline);
       }
     });
-    bot.on('message', msg => {
-      if(msg.author.id === bot.user.id) return;
 
-      if(msg.guild) {
+
+    /**
+     * Listen for messages
+     */
+    bot.on('message', msg => {
+      if (msg.author.id === bot.user.id) return;
+
+      if (msg.guild) {
         // This section may refer to guild-customized content
       }
     });
 
+    /**
+     * Listen for server joining
+     */
     bot.on('guildCreate', guild => {
-      if(!bot.settings.has(guild.id)) {
+      if (!bot.settings.has(guild.id)) {
         bot.settings.set(guild.id, bot.defaultSettings);
       }
     });
 
+    /**
+     * Listen for server removal
+     */
     bot.on("guildDelete", guild => {
       // When the bot leaves or is kicked, delete settings and webhooks to prevent stale entries.
       //bot.webhooks.delete(guild.id);
     });
   }
+}
+
+function loggedInServersToEmbed(bot) {
+  const embed = new Discord.RichEmbed();
+  embed.setColor(0x00ff00);
+  embed.setTitle("Oghmabot Online");
+  embed.setDescription(loggedInServersToString(bot));
+  return embed;
+}
+
+function loggedInServersToString(bot) {
+  return `Logged in to servers: ${Array.from(bot.guilds.values())}`;
 }

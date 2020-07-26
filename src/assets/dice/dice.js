208911b5b488052d23520866086d44c3e3c2e115
aergnist <46282792+aergnist@users.noreply.github.com>
2020-07-26 21:28:34 +0200

    Implement basic roll command, again

--- src/assets/dice/dice.js
+++ src/assets/dice/dice.js
@@ -1,5 +1,22 @@
-const rollSingleDie = die => Math.floor((Math.random() * parseInt(die)) + 1);
+'use strict';
+
+const diceRollRegExp = /^(\d*)(D\d+)((?:[+-]\d*(?:D\d+)?)*)/i;
+
+const anyRoll = (input) => {
+  const baseRoll = input.match(diceRollRegExp);
+  const baseDie = parseInt(baseRoll[2].slice(1));
+  const baseMultiplier = parseInt(baseRoll[1]) || 1;
+
+  let result = 0;
+  for(let i = 0; i < baseMultiplier; i++) {
+    result += rollSingleDie(baseDie);
+  }
+  return result;
+};
+
+const rollSingleDie = (die) => Math.floor((Math.random() * parseInt(die)) + 1);
 
 module.exports = {
+  anyRoll,
   rollSingleDie,
 };
--- src/assets/dice/dice.spec.js
+++ src/assets/dice/dice.spec.js
@@ -1,9 +1,17 @@
 const { rollSingleDie } = require('./dice');
 
 describe('die rolls', () => {
-  it('should return between 1 and 10', () => {
-    const result = rollSingleDie(10);
-    expect(result).toBeGreaterThanOrEqual(1);
-    expect(result).toBeLessThanOrEqual(10);
+
+  describe('anyRoll', () => {
+
   });
+
+  describe('rollSingleDie', () => {
+    it('should return between 1 and 10', () => {
+      const result = rollSingleDie(10);
+      expect(result).toBeGreaterThanOrEqual(1);
+      expect(result).toBeLessThanOrEqual(10);
+    });
+  });
+
 });

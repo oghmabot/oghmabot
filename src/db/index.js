55746e8f6ded34fac6ed50e085f7165934ed83d7
aergnist <46282792+aergnist@users.noreply.github.com>
2020-07-27 17:17:38 +0200

    Try parse again

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,5 @@ const { Client } = require('pg');
 const { parse } = require('pg-connection-string');
 
 module.exports = {
-  connect: () => new Client({
-    ...parse(process.env.DATABASE_URL)
-  }).connect()
+  connect: () => new Client(parse(process.env.DATABASE_URL)).connect()
 };

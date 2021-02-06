7bc4693491d87b56a6694dad91afc0da0ae824ee
aergnist <46282792+aergnist@users.noreply.github.com>
2021-02-06 21:57:34 +0100

    feat(errors): let's output errors to Discord!

--- src/client/events/error.event.ts
+++ src/client/events/error.event.ts
@@ -1,3 +1,22 @@
-export const handleClientError = async (error: Error): Promise<void> => {
+import { TextChannel } from 'discord.js';
+import { OghmabotEmbed } from '../embeds';
+import { OghmabotClient } from '../oghmabot.client';
+
+export const handleClientError = async (client: OghmabotClient, error: Error): Promise<void> => {
+  try {
+    const { BOT_STATUS_CHANNEL } = process.env;
+    if (BOT_STATUS_CHANNEL) {
+      const { channels } = client;
+      const statusChannel = await channels.fetch(BOT_STATUS_CHANNEL) as TextChannel | undefined;
+      if (statusChannel) {
+        statusChannel.send(new OghmabotEmbed({
+          title: error.name,
+          description: error.message,
+        }));
+      }
+    }
+  } catch (err) {
+    console.warn('Failed to output uncaught error to Discord Status Channel.');
+  }
   console.error('Unexpected error.', error);
 };

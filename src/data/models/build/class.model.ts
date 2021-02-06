7de8769935bf5de21135287796e9416e21fec2e3
aergnist <46282792+aergnist@users.noreply.github.com>
2021-02-06 16:37:49 +0100

    chore(class): made the class parsing less complex and made sure all test cases pass correctly

--- src/data/models/build/class.model.ts
+++ src/data/models/build/class.model.ts
@@ -1,4 +1,4 @@
-import { findBestStringMatch } from '../../../utils';
+import { KeepAlphaNumericRegExp, findBestStringMatch } from '../../../utils';
 
 export enum BaseAttackBonusProgression { Half, Medium, Full }
 
@@ -11,13 +11,11 @@ export enum Class {
   Bard,
   Blackguard,
   Cavalier,
-  ChampionOfTorm,
+  ChampionOfTormDivineChampion,
   Cleric,
   Commoner,
-  DivineChampion = ChampionOfTorm,
   Druid,
-  DwarvenDefender,
-  EarthkinDefender = DwarvenDefender,
+  DwarvenDefenderEarthkinDefender,
   FavoredSoul,
   Fighter,
   HarperMage,
@@ -27,7 +25,6 @@ export enum Class {
   Hexblade,
   InvisibleBlade,
   PurpleDragonKnight,
-  Knight = PurpleDragonKnight,
   Loremaster,
   MasterHarper,
   Monk,
@@ -35,7 +32,6 @@ export enum Class {
   PaleMaster,
   Ranger,
   RedDragonDisciple,
-  DragonDisciple = RedDragonDisciple,
   Rogue,
   ShadowMage,
   Shadowdancer,
@@ -58,5 +54,39 @@ export enum Class {
 export const getClass = (str: string): Class | undefined => {
   if (!str) return;
   const nums = [...Array(Object.values(Class).length).keys()];
-  return findBestStringMatch(nums, str, i => Class[i]);
+  switch (str.toLowerCase().replace(KeepAlphaNumericRegExp, '')) {
+    case 'aa': return Class.ArcaneArcher;
+    case 'bb': return Class.Barbarian;
+    case 'b': return Class.Bard;
+    case 'bg': return Class.Blackguard;
+    case 'ct':
+    case 'cot':
+    case 'dc': return Class.ChampionOfTormDivineChampion;
+    case 'cl': return Class.Cleric;
+    case 'dd':
+    case 'ed': return Class.DwarvenDefenderEarthkinDefender;
+    case 'fs': return Class.FavoredSoul;
+    case 'f':
+    case 'ftr': return Class.Fighter;
+    case 'hm':
+    case 'hmage': return Class.HarperMage;
+    case 'hp': return Class.HarperParagon;
+    case 'hpr': return Class.HarperPriest;
+    case 'hs': return Class.HarperScout;
+    case 'ib': return Class.InvisibleBlade;
+    case 'pdk':
+    case 'k': return Class.PurpleDragonKnight;
+    case 'lm': return Class.Loremaster;
+    case 'mh': return Class.MasterHarper;
+    case 'm': return Class.Monk;
+    case 'pm': return Class.PaleMaster;
+    case 'r': return Class.Ranger;
+    case 'rdd': return Class.RedDragonDisciple;
+    case 'rg': return Class.Rogue;
+    case 'sm': return Class.ShadowMage;
+    case 'sd': return Class.Shadowdancer;
+    case 'ss': return Class.Spellsword;
+    case 'wm': return Class.WeaponMaster;
+    default: return findBestStringMatch(nums, str, i => Class[i]);
+  }
 };
